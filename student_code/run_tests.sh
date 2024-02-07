c2c6967a0a3efe44e63f76fb877e447b10e920c1
yasemin-et <yasemin.elif.turkoglu@outlook.com>
2024-02-07 16:48:33 -0500

    Added extra test case

--- student_code/run_tests.sh
+++ student_code/run_tests.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-declare -a input_arr=("input01.txt" "input02.txt")
-declare -a output_arr=("output01.txt" "output02.txt")
 
+declare -a input_arr=("input01.txt" "input02.txt" "input03.txt")
+declare -a output_arr=("output01.txt" "output02.txt" "output03.txt")
 
+# Clean and build the project
 make clean
-if [ $? -eq 0 ]
-Then
+if [ $? -eq 0 ]; then
   echo "Successfully cleaned the project."
 else
   echo "Cleaning the project failed. Terminating the script." >&2
@@ -13,8 +13,7 @@ else
 fi
 
 make all
-if [ $? -eq 0 ]
-then
+if [ $? -eq 0 ]; then
   echo "Successfully built the project."
 else
   echo "Building the project failed. Terminating the script." >&2
@@ -23,23 +22,18 @@ fi
 
 echo "********************************************************************************"
 
-## now loop through the array of inputs
-
-for input_idx in "${!input_arr[@]}"
-  do
-  if [ "$1" = "valgrind" ]
-  then
-    echo valgrind --leak-check=full --show-leak-kinds=all .\/kshell \< "${input_arr[$input_idx]}" 2\>\&1 \| tee "${output_arr[$input_idx]}"
+# Loop through the array of inputs
+for input_idx in "${!input_arr[@]}"; do
+  if [ "$1" = "valgrind" ]; then
+    echo "valgrind --leak-check=full --show-leak-kinds=all ./kshell < \"${input_arr[$input_idx]}\" 2>&1 | tee \"${output_arr[$input_idx]}\""
     valgrind --leak-check=full --show-leak-kinds=all ./kshell < "${input_arr[$input_idx]}" 2>&1 | tee "${output_arr[$input_idx]}"
   else
-    echo .\/kshell \< "${input_arr[$input_idx]}" 2\>\&1 \| tee "${output_arr[$input_idx]}"
+    echo "./kshell < \"${input_arr[$input_idx]}\" 2>&1 | tee \"${output_arr[$input_idx]}\""
     ./kshell < "${input_arr[$input_idx]}" 2>&1 | tee "${output_arr[$input_idx]}"
   fi
-  if [ $? -eq 0 ];
-  then
+  if [ $? -eq 0 ]; then
     echo "Successfully ran the project with input ${input_arr[$input_idx]} and captured output in ${output_arr[$input_idx]}."
-  elif [ $? -eq 137 ];
-  then
+  elif [ $? -eq 137 ]; then
     echo "Running the project with input ${input_arr[$input_idx]} failed due to out of memory condition." >&2
   else
     echo "Running the project with input ${input_arr[$input_idx]} failed." >&2
